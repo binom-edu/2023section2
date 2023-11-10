63ced743a9526cd1ca6951cce6c01b5f3172ff45
klindyuk <klindyuk@gmail.com>
2023-11-10 18:42:21 +0300

    занятие 5

---
+++ 2023-11-07/1.py
@@ -0,0 +1,8 @@
+# даны два числа, найти наибольшее из них
+a = int(input())
+b = int(input())
+if a > b:
+    ans = a
+else:
+    ans = b
+print(ans)
---
+++ 2023-11-07/2.py
@@ -0,0 +1,7 @@
+# даны два числа, найти наибольшее из них
+a = int(input())
+b = int(input())
+ans = a
+if b > ans:
+    ans = b
+print(ans)
